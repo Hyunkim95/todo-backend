6ace73d775f9f7515f11b919113cfe1133239482
Divij <divij.mehra.16@gmail.com>
2017-05-27 23:31:18 +1000

    added mlab

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 
-mongoose.connect('mongodb://localhost/personal-dashboard')
+mongoose.connect(process.env.MONGOLAB_DASH);
   .then(()=>{
     console.log("Connected")
   })
