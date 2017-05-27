0e4d093eead86aefac290d900363dd2a2202169f
Divij <divij.mehra.16@gmail.com>
2017-05-27 23:39:49 +1000

    editted app.js

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.use('/users', users);
 // catch 404 and forward to error handler
 
 mongoose.connect(process.env.MONGOLAB_DASH);
-  .then(()=>{
-    console.log("Connected")
-  })
+  // .then(()=>{
+  //   console.log("Connected")
+  // })
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
